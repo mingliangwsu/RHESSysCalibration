20b37379fa2f462d719e433d9567e4844af1dac4
mingliangwsu <mingliang.liu@wsu.edu>
2023-04-07 16:20:23 -0700

    Add files via upload

--- calibrate_job_mode.sh
+++ calibrate_job_mode.sh
@@ -3,18 +3,24 @@
 #pass arguments: $1: veg type  $2: cover_fraction $3: patchID $4:npp/gpp ratio $5:height $6:npp $7:LAI $8: AGBc
 #$9:cal0_min $10:cal0_max $11:cal1_min $12:cal1_max $13:cal2_min $14:cal2_max $15:cal3_min $16:cal3_max
 
-RHESSys="/home/liuming/RHESSys_Ning/RHESSys/build/Qt/gcc/Release/RHESSys"
-#py_likelyhood="/home/liuming/dev/toolkit/ForRHESSys/growth_yearly_model_performance.py"
-py_likelyhood="/home/liuming/dev/toolkit/ForRHESSys/growth_yearly_model_performance_nppratio_height_npp_LAI_AGBc.py"
+#RHESSys="/home/liuming/RHESSys_Ning/RHESSys/build/Qt/gcc/Release/RHESSys"
+#py_likelyhood="/home/liuming/dev/toolkit/ForRHESSys/growth_yearly_model_performance_nppratio_height_npp_LAI_AGBc.py"
+#droot="/home/liuming/mnt/hydronas2/Projects/FireEarth/Cedar"
+#patch_stata_veg="${droot}/patch_stratum_vegid.txt"
 
-droot="/home/liuming/mnt/hydronas3/Projects/FireEarth/for_min/scenarios/historical/gridmet/1979"
-patch_stata_veg="${droot}/patch_stratum_vegid.txt"
 mywkdir="$PWD"
 cd ${droot}
 
 cal_veg=$1 #evergreen
 cal_veg_coverfraction=$2
 cal_patch=$3
+
+flowtable=patch_${cal_patch}.flow
+worldfile=patch_${cal_patch}.state
+
+
+
+
 
 if [ "$cal_veg" = "1" ]; then
     source_default=${droot}/defs/stratum_evergreen_mode.def
@@ -27,7 +33,7 @@ elif [ "$cal_veg" = "5" ]; then
     target_default=${droot}/defs/stratum_shrub.def
 fi
 
-outputdir=/home/liuming/mnt/hydronas3/Projects/FireEarth/for_min/scenarios/historical/gridmet/1979/Output_calibrations/patch${cal_patch}
+outputdir=${outputdir_root}/patch${cal_patch}
 if [ ! -d "$outputdir" ]; then
     mkdir -p $outputdir 
 fi
@@ -35,20 +41,20 @@ fi
 outpfile=${outputdir}/mrcoef_likelihood_mcmc.txt
 debug_file=${outputdir}/echo_test_likelihood.txt
 
-echo "idx cal0 cal1 cal2 cal3 likelyhood" > $outpfile
+echo "idx cal0 cal1 cal2 cal3 cal4 cal5 likelyhood" > $outpfile
 
 growth_year=${outputdir}/calib_grow_stratum.yearly
 distfile=${outputdir}/dist_npp_gpp_ratio.txt
 logfile=${outputdir}/pylog.txt
 tempGaussRandom=${outputdir}/testGauseRandom.txt
-valid_start_year=2000
-valid_end_year=2015
 valid_veg_id=${cal_veg}
 
 #CAL0 mrc.per_N
 #CAL1 epc.proj_sla
 #CAL2 epc.max_daily_mortality
 #CAL3 epc.dickenson_pa
+#CAL4 epc.height_to_stem_exp
+#CAL5 epc.height_to_stem_coef
 
 #target0: npp/gpp ratio
 #target1: height (m)
@@ -105,10 +111,14 @@ min_c[2]=${13}    #0.005
 max_c[2]=${14}    #0.10
 min_c[3]=${15}    #0.4
 max_c[3]=${16}    #0.8
+min_c[4]=${17}    #0.4
+max_c[4]=${18}    #0.8
+min_c[5]=${19}    #0.4
+max_c[5]=${20}    #0.8
 
 declare -a CAL
 declare -a dtrange
-for cidx in 0 1 2 3;do
+for cidx in 0 1 2 3 4 5;do
     test=$(shuf -i0-100 -n1)
     min=${min_c[$cidx]}
     max=${max_c[$cidx]}
@@ -117,22 +127,26 @@ for cidx in 0 1 2 3;do
     echo cidx:$cidx min:${min} max:${max}
 done
 
-s1=1000.0
-s2=5.241
-po=0.622
-pa=0.50
-gw1=0.2
-gw2=0.049
-flowtable=patch_${cal_patch}.flow
-worldfile=patch_${cal_patch}.state
-tecfile=calibration.tec
-endyear=2015
-stateyear=2015
-spyrs=20
-soil_spins=50
-veg_spins=20 #25
+output_pre=${outputdir}/init_soil
+#Step one: initialize the soil (C & N)
 
-loops=800 #iterations for calibration parameters
+echo "${RHESSys} -netcdfgrid -ncgridinterp 10 -t ${tecfile} -w ${worldfile} -whdr ${headfile} -start_from_zero_soilpools -st ${startyear} 01 01 01 -ed ${endyear} 12 31 24 -pre ${output_pre} -s ${s1} ${s2} -sv ${s1} ${s2} -svalt ${po} ${pa} -gw ${gw1} ${gw2} -r ${flowtable} -g -b -p -c -firespread 100 ${patchgrid} ${demgrid} -Ndecayrate 3.0 -firespin ${spyrs} ${soil_spins} 
+newworldfile=${worldfile}.Y${stateyear}M1D1H1.state"
+${RHESSys} -netcdfgrid -ncgridinterp 10 -t ${tecfile} -w ${worldfile} -whdr ${headfile} -start_from_zero_soilpools -st ${startyear} 01 01 01 -ed ${endyear} 12 31 24 -pre ${output_pre} -s ${s1} ${s2} -sv ${s1} ${s2} -svalt ${po} ${pa} -gw ${gw1} ${gw2} -r ${flowtable} -g -b -p -c -firespread 100 ${patchgrid} ${demgrid} -Ndecayrate 3.0 -firespin ${spyrs} ${soil_spins} 
+
+newworldfile=${worldfile}.Y${stateyear}M1D1H1.state
+worldfile=${newworldfile}
+
+#Step two: init veg -
+output_pre=${outputdir}/init_veg
+
+echo "${RHESSys} -netcdfgrid -ncgridinterp 10 -t ${tecfile} -w ${worldfile} -whdr ${headfile} -start_from_zero_vegpools -st ${startyear} 01 01 01 -ed ${endyear} 12 31 24 -pre ${output_pre} -s ${s1} ${s2} -sv ${s1} ${s2} -svalt ${po} ${pa} -gw ${gw1} ${gw2} -r ${flowtable} -g -b -p -c -firespread 100 ${patchgrid} ${demgrid} -Ndecayrate 3.0 -firespin ${spyrs} ${veg_spins} 
+newworldfile=${worldfile}.Y${stateyear}M1D1H1.state"
+${RHESSys} -netcdfgrid -ncgridinterp 10 -t ${tecfile} -w ${worldfile} -whdr ${headfile} -start_from_zero_vegpools -st ${startyear} 01 01 01 -ed ${endyear} 12 31 24 -pre ${output_pre} -s ${s1} ${s2} -sv ${s1} ${s2} -svalt ${po} ${pa} -gw ${gw1} ${gw2} -r ${flowtable} -g -b -p -c -firespread 100 ${patchgrid} ${demgrid} -Ndecayrate 3.0 -firespin ${spyrs} ${veg_spins} 
+
+newworldfile=${worldfile}.Y${stateyear}M1D1H1.state
+worldfile=${newworldfile}
+
 output_pre=${outputdir}/calib
 
 declare -a old_CAL
@@ -148,27 +162,32 @@ do
     sed -i "s/CAL1/${CAL[1]}/g" $target_default
     sed -i "s/CAL2/${CAL[2]}/g" $target_default
     sed -i "s/CAL3/${CAL[3]}/g" $target_default
+    sed -i "s/CAL4/${CAL[4]}/g" $target_default
+    sed -i "s/CAL5/${CAL[5]}/g" $target_default
     
     #run RHESSys
-    echo "${RHESSys} -netcdfgrid -ncgridinterp 10 -t ${tecfile} -w ${worldfile} -whdr br_with_fire.hdr -start_from_zero_vegpools -st 1980 01 01 01 -ed ${endyear} 12 31 24 -pre ${output_pre} -s ${s1} ${s2} -sv ${s1} ${s2} -svalt ${po} ${pa} -gw ${gw1} ${gw2} -r ${flowtable} -g -b -p -c -firespread 100 ../../../../auxdata/patchgrid.txt ../../../../auxdata/demgrid.txt -Ndecayrate 3.0 -firespin ${spyrs} ${veg_spins}"
-    ${RHESSys} -netcdfgrid -ncgridinterp 10 -t ${tecfile} -w ${worldfile} -whdr br_with_fire.hdr -start_from_zero_vegpools -st 1980 01 01 01 -ed ${endyear} 12 31 24 -pre ${output_pre} -s ${s1} ${s2} -sv ${s1} ${s2} -svalt ${po} ${pa} -gw ${gw1} ${gw2} -r ${flowtable} -g -b -p -c -firespread 100 ../../../../auxdata/patchgrid.txt ../../../../auxdata/demgrid.txt -Ndecayrate 3.0 -firespin ${spyrs} ${veg_spins} 
 
-    #${RHESSys} -netcdfgrid -ncgridinterp 10 -t calibration.tec -w world_file_patchID_100951_100952.txt -whdr  defFiles.hdr  -st 1979 01 01 01 -ed 2015 12 31 24 -pre ./Outputs/spinup_fire_ -s 5.200298 54.26672 -sv 5.200298 54.26672 -svalt 1.143066 3.829007 -gw 0.604857 0.3806356 -r flowtable_100951_100952.txt -b -g -p -c -h -z -firespread 100 patchGrid.txt DemGrid.txt -firemort 0.71 -9999  -9999 0.52 4.05 -10 1 -firespin 10 50
-    
+
+
+
+    echo "${RHESSys} -netcdfgrid -ncgridinterp 10 -t ${tecfile} -w ${worldfile} -whdr ${headfile} -start_from_zero_vegpools -st ${startyear} 01 01 01 -ed ${endyear} 12 31 24 -pre ${output_pre} -s ${s1} ${s2} -sv ${s1} ${s2} -svalt ${po} ${pa} -gw ${gw1} ${gw2} -r ${flowtable} -g -b -p -c -firespread 100 ${patchgrid} ${demgrid} -Ndecayrate 3.0 -firespin ${spyrs} ${veg_spins}"
+    ${RHESSys} -netcdfgrid -ncgridinterp 10 -t ${tecfile} -w ${worldfile} -whdr ${headfile} -start_from_zero_vegpools -st ${startyear} 01 01 01 -ed ${endyear} 12 31 24 -pre ${output_pre} -s ${s1} ${s2} -sv ${s1} ${s2} -svalt ${po} ${pa} -gw ${gw1} ${gw2} -r ${flowtable} -g -b -p -c -firespread 100 ${patchgrid} ${demgrid} -Ndecayrate 3.0 -firespin ${spyrs} ${veg_spins} 
+
+  
     #-firespin 10 50
     #cal likelihood (squre dist)
-    echo "python ${py_likelyhood} ${growth_year} ${valid_start_year} ${valid_end_year} ${valid_veg_id} ${patch_stata_veg} ${target[0]} ${target_stddev[0]} ${target[1]} ${target_stddev[1]} ${target[2]} ${target_stddev[2]} ${target[3]} ${target_stddev[3]} ${target[4]} ${target_stddev[4]} ${distfile}"
+    echo "python ${py_likelyhood} ${growth_year} ${valid_start_year} ${valid_end_year} ${valid_veg_id} ${patch_stata_veg} ${target[0]} ${target_stddev[0]} ${target[1]} ${target_stddev[1]} ${target[2]} ${target_stddev[2]} ${target[3]} ${target_stddev[3]} ${target[4]} ${target_stddev[4]} ${distfile} ${logfile}"
     #read -p 'Press [Enter] key to continue...'
-    printf "%d %.4f %.4f %.4f %.4f %.4e\n" $idx ${CAL[0]} ${CAL[1]} ${CAL[2]} ${CAL[3]} ${likelihood} >> $logfile
+    printf "%d %.4f %.4f %.4f %.4f %.4f %.4f %.4e\n" $idx ${CAL[0]} ${CAL[1]} ${CAL[2]} ${CAL[3]} ${CAL[4]} ${CAL[5]} ${likelihood} >> $logfile
     python ${py_likelyhood} ${growth_year} ${valid_start_year} ${valid_end_year} ${valid_veg_id} ${patch_stata_veg} ${target[0]} ${target_stddev[0]} ${target[1]} ${target_stddev[1]} ${target[2]} ${target_stddev[2]} ${target[3]} ${target_stddev[3]} ${target[4]} ${target_stddev[4]} ${distfile} ${logfile}
 
     likelihood=$(head -n 1 ${distfile})
     if [ $idx -eq 1 ]; then
         old_likelihood=${likelihood}
         #tv=`echo "scale=4;$idx $CALq10 $CALper_N ${likelihood}" | bc -l`
-        printf "%d %.4f %.4f %.4f %.4f %.4e\n" $idx ${CAL[0]} ${CAL[1]} ${CAL[2]} ${CAL[3]} ${likelihood} >> $outpfile
+        printf "%d %.4f %.4f %.4f %.4f %.4f %.4f %.4e\n" $idx ${CAL[0]} ${CAL[1]} ${CAL[2]} ${CAL[3]} ${CAL[4]} ${CAL[5]} ${likelihood} >> $outpfile
         update=1
-        for cidx in 0 1 2 3;do
+        for cidx in 0 1 2 3 4 5;do
             old_CAL[$cidx]=${CAL[$cidx]}
         done
     else
@@ -176,15 +195,15 @@ do
         beta=`ps -ef | awk -v a=${likelihood} -v b=${old_likelihood} 'BEGIN{print (a / b)}'`
         u=$(shuf -i0-100 -n1)
         ru=`echo "scale=6; ${u}*0.01" | bc`
-        echo idx:$idx beta:${beta} ru:${ru} perf:${likelihood} old_perf:${old_likelihood} cal0:${CAL[0]} cal1:${CAL[1]} cal2:${CAL[2]} cal3:${CAL[3]} >> $debug_file
+        echo idx:$idx beta:${beta} ru:${ru} perf:${likelihood} old_perf:${old_likelihood} cal0:${CAL[0]} cal1:${CAL[1]} cal2:${CAL[2]} cal3:${CAL[3]} cal4:${CAL[4]} cal5:${CAL[5]} >> $debug_file
         if (( $(echo "${ru} <= ${beta}" | bc -l) ))
         then
             #accept
             update=1
             echo accept! >> $debug_file
-            printf "%d %.4f %.4f %.4f %.4f %.4e\n" $idx ${CAL[0]} ${CAL[1]} ${CAL[2]} ${CAL[3]} ${likelihood} >> $outpfile
+            printf "%d %.4f %.4f %.4f %.4f %.4f %.4f %.4e\n" $idx ${CAL[0]} ${CAL[1]} ${CAL[2]} ${CAL[3]} ${CAL[4]} ${CAL[5]} ${likelihood} >> $outpfile
             old_likelihood=${likelihood}
-            for cidx in 0 1 2 3;do
+            for cidx in 0 1 2 3 4 5;do
                 old_CAL[$cidx]=${CAL[$cidx]}
             done
         else
@@ -202,7 +221,7 @@ do
     #old_CAL2=${CAL2}
 
     #generate proposal
-    for cidx in 0 1 2 3;do
+    for cidx in 0 1 2 3 4 5;do
         python /home/liuming/dev/toolkit/ForRHESSys/generate_gauss_random.py 0 ${dtrange[${cidx}]} $tempGaussRandom
         dt[${cidx}]=$(head -n 1 ${tempGaussRandom})
         ldt=${dt[${cidx}]}
@@ -220,7 +239,7 @@ do
         fi
     done
     
-    echo idx:$idx old_cal0:${old_CAL[0]} proposed_cal0:${CAL[0]} dt0:${dt[0]} old_cal1:${old_CAL[1]} proposed_cal1:${CAL[1]} dt1:${dt[1]} old_cal2:${old_CAL[2]} proposed_cal2:${CAL[2]} dt2:${dt[2]} >> $debug_file
+    echo idx:$idx old_cal0:${old_CAL[0]} proposed_cal0:${CAL[0]} dt0:${dt[0]} old_cal1:${old_CAL[1]} proposed_cal1:${CAL[1]} dt1:${dt[1]} old_cal2:${old_CAL[2]} proposed_cal2:${CAL[2]} dt2:${dt[2]} old_cal3:${old_CAL[3]} proposed_cal3:${CAL[3]} dt3:${dt[3]}  old_cal4:${old_CAL[4]} proposed_cal4:${CAL[4]} dt4:${dt[4]}  old_cal5:${old_CAL[5]} proposed_cal5:${CAL[5]} dt5:${dt[5]} >> $debug_file
     
     idx=$(( $idx + 1 ))
 done
